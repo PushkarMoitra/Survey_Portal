e5162a073dd838037a5a3aac2dabae58f1885d30
P_Moitra <P_Moitra@Predator>
2021-04-18 16:32:36 +0530

    Minor changes.

--- com-cg-surveyportal/src/main/java/com/cg/surveyportal/SurveyportalApplication.java
+++ com-cg-surveyportal/src/main/java/com/cg/surveyportal/SurveyportalApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SurveyportalApplication {
-
 	public static void main(String[] args) {
-	
 		SpringApplication.run(SurveyportalApplication.class, args);
 	}
 }
